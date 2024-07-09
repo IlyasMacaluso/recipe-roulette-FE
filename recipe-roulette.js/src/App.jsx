90f50e733e2a554d4bf9960d6be6b140cea9e996
IlyasMacaluso <ilyas.macaluso@gmail.com>
2024-07-09 16:27:55 +0200

    da sistemare

--- recipe-roulette.js/src/App.jsx
+++ recipe-roulette.js/src/App.jsx
@@ -1,14 +1,67 @@
-import { RouterProvider, createRouter } from "@tanstack/react-router"
-import { routeTree } from "./routeTree.gen"
+import { createRouter, Outlet, RouterProvider } from "@tanstack/react-router";
+import { AuthProvider } from "./components/authentication/AuthContext";
+import { RecipesProvider } from "./contexts/RecipesContext";
+import { IngredientsProvider } from "./pages/Roulette/IngredientsContext";
+import { RecipesFetchProvider } from "./hooks/useRecipesFetch/useRecipesFetch";
+import { SnackbarProvider } from "./components/Snackbar/useSnackbar";
+import { Header } from "./components/Header/Header";
+import { SideMenu } from "./components/SideMenu/SideMenu";
+import { SideBarRecipes } from "./components/Sidebar/SideBarRecipes";
+import { Sidebar } from "./components/Sidebar/Sidebar";
+import { Snackbar } from "./components/Snackbar/Snackbar";
+import { useDiscoverySidebar } from "./hooks/DiscoverySidebar/useDiscoverySidebar";
+import { useSideMenu } from "./hooks/SideMenu/useSideMenu";
+import { useRecipesResultsSideBar } from "./hooks/RecipesResultsSideBar/useRecipesResultsSideBar";
+import { routeTree } from "./routeTree.gen";
+import { Route } from "../src/routes/__root.jsx";
 
-const router = createRouter({ routeTree })
+const router = createRouter({ routeTree });
 
 function App() {
+    const { handleSidebarToggle, sidebarState } = useDiscoverySidebar();
+    const { handleMenuToggle, path, menuState } = useSideMenu();
+    const { toggleSidebarRecipes, sideBarState } = useRecipesResultsSideBar();
+
     return (
         <div className="appContainer">
-            <RouterProvider router={router} />
+                <RouterProvider router={router}>
+                <Route
+                    contextProviders={
+                        <AuthProvider>
+                            <RecipesProvider>
+                                <IngredientsProvider>
+                                    <RecipesFetchProvider>
+                                        <SnackbarProvider>
+                                            <SideMenu
+                                                handleMenuToggle={handleMenuToggle}
+                                                menuState={menuState}
+                                                path={path}
+                                            />
+                                            <SideBarRecipes
+                                                state={sideBarState}
+                                                toggleSidebarRecipes={toggleSidebarRecipes}
+                                            />
+                                            <Sidebar
+                                                sidebarState={sidebarState}
+                                                handleSidebarToggle={handleSidebarToggle}
+                                            />
+                                            <Header
+                                                handleRecipesSidebarToggle={toggleSidebarRecipes}
+                                                handleSidebarToggle={handleSidebarToggle}
+                                                handleMenuToggle={handleMenuToggle}
+                                            />
+                                            <Outlet />
+                                            <Snackbar />
+                                        </SnackbarProvider>
+                                    </RecipesFetchProvider>
+                                </IngredientsProvider>
+                            </RecipesProvider>
+                        </AuthProvider>
+                    }
+                />
+            </RouterProvider>
         </div>
-    )
+    );
 }
 
-export default App
+export default App;
--- recipe-roulette.js/src/routes/__root.jsx
+++ recipe-roulette.js/src/routes/__root.jsx
@@ -1,52 +1,13 @@
-import { Outlet } from "@tanstack/react-router";
-import { createRootRoute } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
-import { IngredientsProvider } from "../pages/Roulette/IngredientsContext";
-import { RecipesProvider } from "../contexts/RecipesContext";
-import { AuthProvider } from "../components/authentication/AuthContext";
-import { RecipesFetchProvider } from "../hooks/useRecipesFetch/useRecipesFetch";
-import { SnackbarProvider } from "../components/Snackbar/useSnackbar";
-import { Header } from "../components/Header/Header";
-import { SideMenu } from "../components/SideMenu/SideMenu";
-import { SideBarRecipes } from "../components/Sidebar/SideBarRecipes";
-import { Sidebar } from "../components/Sidebar/Sidebar";
-import { useDiscoverySidebar } from "../hooks/DiscoverySidebar/useDiscoverySidebar";
-import { useSideMenu } from "../hooks/SideMenu/useSideMenu";
-import { useRecipesResultsSideBar } from "../hooks/RecipesResultsSideBar/useRecipesResultsSideBar";
-import { Snackbar } from "../components/Snackbar/Snackbar";
+import { Outlet, createRootRoute } from "@tanstack/react-router"
+import { TanStackRouterDevtools } from "@tanstack/router-devtools"
 
 export const Route = createRootRoute({
-    component: () => {
-        const { handleSidebarToggle, sidebarState } = useDiscoverySidebar();
-        const { handleMenuToggle, path, menuState } = useSideMenu();
-        const { toggleSidebarRecipes, sideBarState } = useRecipesResultsSideBar();
-
+    component: ({ contextProviders }) => {
         return (
             <>
-                <AuthProvider>
-                    <RecipesProvider>
-                        <IngredientsProvider>
-                            <RecipesFetchProvider>
-                                <SnackbarProvider>
-                                    <SideMenu handleMenuToggle={handleMenuToggle} menuState={menuState} path={path} />
-                                    <SideBarRecipes state={sideBarState} toggleSidebarRecipes={toggleSidebarRecipes} />
-                                    <Sidebar sidebarState={sidebarState} handleSidebarToggle={handleSidebarToggle} />
-
-                                    <Header
-                                        handleRecipesSidebarToggle={toggleSidebarRecipes}
-                                        handleSidebarToggle={handleSidebarToggle}
-                                        handleMenuToggle={handleMenuToggle}
-                                    />
-
-                                    <Outlet />
-                                    <Snackbar />
-                                    <TanStackRouterDevtools />
-                                </SnackbarProvider>
-                            </RecipesFetchProvider>
-                        </IngredientsProvider>
-                    </RecipesProvider>
-                </AuthProvider>
+                {contextProviders}
+                <TanStackRouterDevtools />
             </>
-        );
+        )
     },
-});
+})
