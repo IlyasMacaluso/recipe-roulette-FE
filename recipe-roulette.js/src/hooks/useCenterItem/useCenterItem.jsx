7a66de70f2a882c54ff330c265313419dfe8d0f0
IlyasMacaluso <ilyas.macaluso@gmail.com>
2024-07-15 12:28:50 +0200

    read prev commit

--- recipe-roulette.js/src/hooks/useCenterItem/useCenterItem.jsx
+++ recipe-roulette.js/src/hooks/useCenterItem/useCenterItem.jsx
@@ -2,8 +2,9 @@ import { useEffect, useRef, useState } from "react"
 
 export const useCenterItem = (n) => {
     const [isKeyboardOpen, setIsKeyboardOpen] = useState(false)
-    const refs = Array.from({ length: n }, () => useRef(null)) // crea un array di refs di lunghezza parai al parametro "n"
+    const refs = Array.from({ length: n }, () => useRef(null)) // create an array of refs, with length equal to the "n" parameter
 
+    //determine if the keyboard is opened or not
     useEffect(() => {
         const screenHeight = window.screen.height
 
@@ -24,18 +25,17 @@ export const useCenterItem = (n) => {
         }
     }, [])
 
-
     const scrollToCenter = (currentRef) => {
-
         const scrollItem = () => {
             currentRef.current.scrollIntoView({
                 behavior: "smooth",
                 block: "center",
             })
         }
-
+        // if the keyboard is opened, the item will be centered immediately
         if (isKeyboardOpen) {
             scrollItem()
+        // if the keyboard is closed the item will be centered after a 200ms delay, (wait the new window.innerHeight before the item is centered)
         } else {
             setTimeout(() => scrollItem(), 200)
         }
