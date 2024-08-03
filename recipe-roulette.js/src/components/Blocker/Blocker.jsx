f98f3272ef8f485b56f4f37466941cd5cdb9e6ba
IlyasMacaluso <ilyas.macaluso@gmail.com>
2024-08-03 14:08:11 +0200

    read previous commit

--- recipe-roulette.js/src/components/Blocker/Blocker.jsx
+++
@@ -1,8 +0,0 @@
-import { useState } from "react"
-import { Block } from "@tanstack/react-router"
-
-export function Blocker() {
-    const [blockCondition, setBlockCondition] = useState(false)
-    return <Block blockerFn={() => windows.confirm("Are you sure you want to leave?")} condition={blockCondition} />
-}
-
